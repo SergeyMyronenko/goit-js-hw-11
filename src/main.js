183e3ec1679042a32a11b94f9aafc19ef68f63c5
Myronenlo <55333290+SergeyMyronenko@users.noreply.github.com>
2023-12-31 15:59:52 +0200

    add loader

--- src/main.js
+++ src/main.js
@@ -3,46 +3,82 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-const searchParams = new URLSearchParams({
+const form = document.querySelector('.form');
+const gallery = document.querySelector('.gallery');
+const galleryBox = document.querySelector('.gallery-box');
+const loader = document.querySelector('.loader');
+const input = document.querySelector('input');
+
+let paramsInfo = {
   key: '37773269-50f55f614e71cb99e92638715',
   q: 'black',
   image_type: 'photo',
   orientation: 'horizontal',
   safesearch: true,
-});
+};
 
-fetch(`https://pixabay.com/api/?${searchParams}`)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(
-        'Sorry, there are no images matching your search query. Please try again!'
-      );
-    }
-    return response.json();
-  })
-  .then(images => {
-    console.log(images);
-    const gallery = document.querySelector('.gallery');
-
-    const renderImg = images.reduce((html, image) => {
-      return (
-        html +
-        `<li class="gallery-item">
-        <a href=${image.largeImageURL}> 
-          <img src =${image.webformatURL} alt=${image.tags}/>
+function searchImg(params) {
+  loader.style.display = 'block';
+  gallery.style.display = 'none';
+  setTimeout(() => {
+    fetch(`https://pixabay.com/api/?${params}`)
+      .then(response => {
+        loader.style.display = 'none';
+        gallery.style.display = 'flex';
+        if (!response.ok) {
+          throw new Error(error.status);
+        }
+        return response.json();
+      })
+      .then(({ hits }) => {
+        if (hits.length > 0) {
+          const renderImg = hits.reduce((html, hit) => {
+            return (
+              html +
+              `<li class="gallery-item">
+        <a href=${hit.largeImageURL}> 
+          <img class="gallery-img" src =${hit.webformatURL} alt=${hit.tags}/>
         </a>
-      <p>Likes: ${image.likes}</p>
-      <p>views: ${image.views}</p>
-      <p>comments: ${image.comments}</p>
-      <p>downloads: ${image.downloads}</p>
+        <div class="gallery-text-box">
+          <p>Likes: <span class="text-value">${hit.likes}</span></p>
+          <p>views: <span class="text-value">${hit.views}</span></p>
+          <p>comments: <span class="text-value">${hit.comments}</span></p>
+          <p>downloads: <span class="text-value">${hit.downloads}</span></p>
+      </div>
       </li>`
-      );
-    }, '');
-    gallery.innerHTML = renderImg;
-  })
-  .catch(error => {
-    iziToast.error({
-      position: 'bottomCenter',
-      message: error.message,
-    });
-  });
+            );
+          }, '');
+          gallery.innerHTML = renderImg;
+          new SimpleLightbox('.gallery a', {
+            nav: true,
+            captionDelay: 250,
+            captionsData: 'alt',
+            close: true,
+            enableKeyboard: true,
+            docClose: true,
+          });
+        } else {
+          gallery.style.display = 'none';
+          iziToast.error({
+            position: 'topRight',
+            message:
+              'Sorry, there are no images matching your search query. Please try again!',
+          });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, 2000);
+}
+
+form.addEventListener('click', event => {
+  event.preventDefault();
+
+  if (event.target.type === 'submit') {
+    paramsInfo.q = input.value;
+    const searchParams = new URLSearchParams(paramsInfo);
+    searchImg(searchParams);
+  }
+  form.reset();
+});
