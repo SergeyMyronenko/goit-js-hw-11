352056131da23ce8e7f0638e3c4da4cc9811735a
Myronenlo <55333290+SergeyMyronenko@users.noreply.github.com>
2024-01-03 11:45:29 +0200

    rename build

--- src/main.js
+++ src/main.js
@@ -20,22 +20,21 @@ let paramsInfo = {
 function searchImg(params) {
   loader.style.display = 'block';
   gallery.style.display = 'none';
-  setTimeout(() => {
-    fetch(`https://pixabay.com/api/?${params}`)
-      .then(response => {
-        loader.style.display = 'none';
-        gallery.style.display = 'flex';
-        if (!response.ok) {
-          throw new Error(error.status);
-        }
-        return response.json();
-      })
-      .then(({ hits }) => {
-        if (hits.length > 0) {
-          const renderImg = hits.reduce((html, hit) => {
-            return (
-              html +
-              `<li class="gallery-item">
+  fetch(`https://pixabay.com/api/?${params}`)
+    .then(response => {
+      loader.style.display = 'none';
+      gallery.style.display = 'flex';
+      if (!response.ok) {
+        throw new Error(error.status);
+      }
+      return response.json();
+    })
+    .then(({ hits }) => {
+      if (hits.length > 0) {
+        const renderImg = hits.reduce((html, hit) => {
+          return (
+            html +
+            `<li class="gallery-item">
         <a href=${hit.largeImageURL}> 
           <img class="gallery-img" src =${hit.webformatURL} alt=${hit.tags}/>
         </a>
@@ -46,30 +45,29 @@ function searchImg(params) {
           <p>downloads: <span class="text-value">${hit.downloads}</span></p>
       </div>
       </li>`
-            );
-          }, '');
-          gallery.innerHTML = renderImg;
-          new SimpleLightbox('.gallery a', {
-            nav: true,
-            captionDelay: 250,
-            captionsData: 'alt',
-            close: true,
-            enableKeyboard: true,
-            docClose: true,
-          });
-        } else {
-          gallery.style.display = 'none';
-          iziToast.error({
-            position: 'topRight',
-            message:
-              'Sorry, there are no images matching your search query. Please try again!',
-          });
-        }
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, 2000);
+          );
+        }, '');
+        gallery.innerHTML = renderImg;
+        new SimpleLightbox('.gallery a', {
+          nav: true,
+          captionDelay: 250,
+          captionsData: 'alt',
+          close: true,
+          enableKeyboard: true,
+          docClose: true,
+        });
+      } else {
+        gallery.style.display = 'none';
+        iziToast.error({
+          position: 'topRight',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    });
 }
 
 form.addEventListener('click', event => {
